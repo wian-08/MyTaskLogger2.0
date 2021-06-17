b09b64fca00286e372b9c72f1e80812677792e70
wian-08 <79966607+wian-08@users.noreply.github.com>
2021-06-17 19:15:23 +0200

    added ToDoEdit

--- mytasklogger2.0/src/components/HelloWorld.vue
+++ mytasklogger2.0/src/components/HelloWorld.vue
@@ -1,104 +1,87 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <p class="no-print">ToDo Item: <input v-model="name" /></p>
+    <p>{{ name }}</p>
+
+    <div v-for="item in info" :key="item.id">
+      {{ item.name }} -
+      <button type="button" v-on:click="deleteToDo(item.id)">Delete</button> |
+      <!-- <button type="button" v-on:click="editToDo(item)">Edit</button> --> 
+      <router-link :to="{ name: 'Edit', params: {id: item.id }}">Edit</router-link>
+    </div>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+      <button type="button" v-on:click="writeToDo()">Auto Add ToDo</button>
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+
+    <div v-if="editSeen" id="hide">
+      <p>hide me</p>
+      <input type="hidden" v-model="editItem.id" /><br />
+      <input type="text" v-model="editItem.name" />
+      <button type="button" v-on:click="updateToDo(editItem)">
+        Update ToDo
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+//import VueAxios from "vue-axios";
+
 export default {
   name: "HelloWorld",
+  data() {
+    return {
+      name: "",
+      info: null,
+      editSeen: false,
+      editItem: null,
+    };
+  },
   props: {
     msg: String,
+  },
+  mounted() {
+    this.getToDo();
+  },
+  methods: {
+    getToDo() {
+      axios
+        .get("http://metistestapi.daks.co.za/api/ToDoItem")
+        .then((response) => (this.info = response.data));
+    },
+    writeToDo() {
+      const todo = { name: this.name };
+      axios
+        .post("http://metistestapi.daks.co.za/api/ToDoItem", todo)
+        .then((response) => {
+          return response, this.getToDo();
+        });
+    },
+    editToDo: function (editItem) {
+      this.editItem = editItem;
+      this.editSeen = true;
+    },
+    updateToDo: function (editItem) {
+      axios
+        .put(
+          "http://metistestapi.daks.co.za/api/ToDoItem/" + editItem.id,
+          editItem
+        )
+        .then((response) => {
+          return response, this.getToDo();
+        });
+      this.editSeen = false;
+    },
+    deleteToDo: function (id) {
+      axios
+        .delete("http://metistestapi.daks.co.za/api/ToDoItem/" + id)
+        .then((response) => {
+          return response, this.getToDo();
+        });
+    },
   },
 };
 </script>
